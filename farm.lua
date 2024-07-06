2bdc2cb62a37f59e426728bd22b5257439813ae8
SkrenJ <marius.janowski@gmail.com>
2024-07-06 22:31:45 +0200

    fix

--- farm.lua
+++ farm.lua
@@ -1,91 +1,92 @@
 -- Funktion zum Ernten und Pflanzen von Weizen, Tomaten oder Mais auf einem Feld
 function farmField()
-    local stepsX = 0  -- Zähler für Schritte in X-Richtung
-    local stepsZ = 0  -- Zähler für Schritte in Z-Richtung
-    local forward = true  -- Richtung der Bewegung in X-Richtung (true = vorwärts, false = rückwärts)
-  
-    for z = 0, 8 do  -- Schleife über die Z-Achse (9 Blöcke)
-      for x = 0, 8 do  -- Schleife über die X-Achse (9 Blöcke)
-        farmCrop("minecraft:wheat", 7, 1, 1)  -- Weizen ernten und pflanzen
-        farmCrop("thermal:tomato", 10, 2, 1)  -- Tomaten ernten und pflanzen
-        farmCrop("thermal:corn", 9, 3, 2)     -- Mais ernten und pflanzen
-        farmCrop("thermal:eggplant", 10, 4, 1) -- Auberginen ernten und pflanzen
-  
-        -- Schritte in X-Richtung zählen und bewegen
-        if x < 8 then
-          if forward then
-            turtle.forward()
-            stepsX = stepsX + 1
-          else
-            turtle.back()
-            stepsX = stepsX - 1
-          end
+    local stepsX = 0                               -- Zähler für Schritte in X-Richtung
+    local stepsZ = 0                               -- Zähler für Schritte in Z-Richtung
+    local forward = true                           -- Richtung der Bewegung in X-Richtung (true = vorwärts, false = rückwärts)
+
+    for z = 0, 8 do                                -- Schleife über die Z-Achse (9 Blöcke)
+        for x = 0, 8 do                            -- Schleife über die X-Achse (9 Blöcke)
+            farmCrop("minecraft:wheat", 7, 1, 1)   -- Weizen ernten und pflanzen
+            farmCrop("thermal:tomato", 10, 2, 1)   -- Tomaten ernten und pflanzen
+            farmCrop("thermal:corn", 9, 3, 2)      -- Mais ernten und pflanzen
+            farmCrop("thermal:eggplant", 10, 4, 1) -- Auberginen ernten und pflanzen
+
+            -- Schritte in X-Richtung zählen und bewegen
+            if x < 8 then
+                if forward then
+                    turtle.forward()
+                    stepsX = stepsX + 1
+                else
+                    turtle.back()
+                    stepsX = stepsX - 1
+                end
+            end
         end
-      end
-  
-      -- Schritte in Z-Richtung zählen und bewegen
-      if z < 8 then
-        turtle.turnRight()
-        turtle.forward()
-        turtle.turnLeft()
-        stepsZ = stepsZ + 1
-      end
-  
-      -- Richtung der Bewegung in X-Richtung umkehren
-      forward = not forward
+
+        -- Schritte in Z-Richtung zählen und bewegen
+        if z < 8 then
+            turtle.turnRight()
+            turtle.forward()
+            turtle.turnLeft()
+            stepsZ = stepsZ + 1
+        end
+
+        -- Richtung der Bewegung in X-Richtung umkehren
+        forward = not forward
     end
-  
+
     -- Zurück zur Startposition
-    for i = 1, stepsZ do  -- Schritte in Z-Richtung rückgängig machen
-      turtle.turnLeft()
-      turtle.forward()
-      turtle.turnRight()
+    for i = 1, stepsZ do -- Schritte in Z-Richtung rückgängig machen
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnRight()
     end
-  
+
     -- Schritte in X-Richtung rückgängig machen (in umgekehrter Richtung)
-    if not forward then  -- Wenn die letzte Bewegung rückwärts war, jetzt vorwärts fahren
-      turtle.turnRight()
-      turtle.turnRight()
+    if not forward then -- Wenn die letzte Bewegung rückwärts war, jetzt vorwärts fahren
+        turtle.turnRight()
+        turtle.turnRight()
     end
     for i = 1, stepsX do
-      turtle.forward()
+        turtle.forward()
     end
-  end
-
-
+end
 
 -- Funktion zum Ernten und Pflanzen einer bestimmten Pflanze
 function farmCrop(cropName, matureAge, seedSlot, height)
-    local currentY = 0 -- Variable to keep track of the current Y level
-  
+    local currentY = 0
+    
     for i = 1, height do
       local success, blockInfo = turtle.inspectDown()
       if success and blockInfo.name == cropName then
-        if blockInfo.state.age == matureAge then
+        if blockInfo.state.age == matureAge then -- Nur ernten, wenn die Pflanze reif ist
           turtle.digDown()
+        else
+          break -- Schleife abbrechen, wenn die Pflanze nicht reif ist
         end
       end
   
-      -- Move up only if not already at the top
-      if i < height and currentY < height - 1 then 
+      if i < height and currentY < height - 1 then
         turtle.up()
         currentY = currentY + 1
       end
     end
   
-    turtle.select(seedSlot)
-    turtle.placeDown()
+    -- Nur pflanzen, wenn die Pflanze geerntet wurde (d.h. die Schleife nicht vorzeitig abgebrochen wurde)
+    if i == height then 
+      turtle.select(seedSlot)
+      turtle.placeDown()
+      for i = 1, height - 1 do
+        turtle.up()
+        turtle.placeUp()
+      end
+      print(cropName .. " geerntet und neu gepflanzt")
+    end
   
     -- Move down to the original Y level
     for i = 1, currentY do
       turtle.down()
     end
-  
-    for i = 1, height - 1 do
-      turtle.up()
-      turtle.placeUp()
-    end
-    print(cropName .. " geerntet und neu gepflanzt")
   end
   
 
