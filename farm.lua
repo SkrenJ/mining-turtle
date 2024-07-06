355e258c400c778ad52a034e7c64d365617e881a
SkrenJ <marius.janowski@gmail.com>
2024-07-06 23:04:41 +0200

    fix

--- farm.lua
+++ farm.lua
@@ -2,18 +2,18 @@
 function farmField()
     local stepsX = 0
     local stepsZ = 0
-    local forward = false -- Startrichtung nach Westen (-x)
+    local forward = true -- Startrichtung nach Osten (+x)
 
-    for z = 8, 0, -1 do   -- Schleife über die Z-Achse (9 Blöcke, absteigend)
-        stepsX = 0        -- Zähler für X-Richtung am Anfang jeder Reihe zurücksetzen
-        for x = 8, 0, -1 do -- Schleife über die X-Achse (9 Blöcke, absteigend)
+    for z = 0, 8 do      -- Schleife über die Z-Achse (9 Blöcke)
+        stepsX = 0       -- Zähler für X-Richtung am Anfang jeder Reihe zurücksetzen
+        for x = 0, 8 do  -- Schleife über die X-Achse (9 Blöcke)
             farmCrop("minecraft:wheat", 7, 1, 1)
             farmCrop("thermal:tomato", 10, 2, 1)
             farmCrop("thermal:corn", 9, 3, 2)
             farmCrop("thermal:eggplant", 10, 4, 1)
 
             -- Schritte in X-Richtung zählen und bewegen (nur wenn nicht am Ende der Reihe)
-            if x > 0 then
+            if x < 8 then
                 if forward then
                     turtle.forward()
                     stepsX = stepsX + 1
@@ -28,23 +28,23 @@ function farmField()
         forward = not forward
 
         -- Schritte in Z-Richtung zählen und bewegen (nur wenn nicht am Ende des Feldes)
-        if z > 0 then
-            turtle.turnLeft() -- Immer nach links drehen, da wir in -z Richtung gehen
+        if z < 8 then
+            turtle.turnRight() -- Immer nach rechts drehen, da wir in +z Richtung gehen
             turtle.forward()
-            turtle.turnRight()
+            turtle.turnLeft()
             stepsZ = stepsZ + 1
         end
     end
 
     -- Zurück zur Startposition
     for i = 1, stepsZ do
-        turtle.turnRight() -- Immer nach rechts drehen, da wir zurück in +z Richtung gehen
+        turtle.turnLeft() -- Immer nach links drehen, da wir zurück in -z Richtung gehen
         turtle.forward()
-        turtle.turnLeft()
+        turtle.turnRight()
     end
 
     -- Schritte in X-Richtung rückgängig machen (in umgekehrter Richtung)
-    if forward then
+    if not forward then
         turtle.turnRight()
         turtle.turnRight()
     end
@@ -52,6 +52,18 @@ function farmField()
         turtle.forward()
     end
 end
+
+-- Funktion zum Ernten und Pflanzen einer bestimmten Pflanze (unverändert)
+function farmCrop(cropName, matureAge, seedSlot, height)
+    -- ... (Code aus vorheriger Antwort) ...
+end
+
+-- Hauptprogramm
+while true do
+    farmField()
+    sleep(10) -- Wartezeit zwischen den Durchläufen
+end
+
 
 -- Funktion zum Ernten und Pflanzen einer bestimmten Pflanze
 function farmCrop(cropName, matureAge, seedSlot, height)
