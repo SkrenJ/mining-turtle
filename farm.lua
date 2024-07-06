7c7a5022f605e612dfac4ac4dad79b6592ea454a
SkrenJ <marius.janowski@gmail.com>
2024-07-06 23:14:59 +0200

    fix

--- farm.lua
+++ farm.lua
@@ -4,15 +4,14 @@ function farmField()
     local stepsZ = 0
     local forward = true -- Startrichtung nach Osten (+x)
 
-    for z = 0, 8 do      -- Schleife über die Z-Achse (9 Blöcke)
-        stepsX = 0       -- Zähler für X-Richtung am Anfang jeder Reihe zurücksetzen
-        for x = 0, 8 do  -- Schleife über die X-Achse (9 Blöcke)
+    for z = 0, 8 do
+        stepsX = 0
+        for x = 0, 8 do
             farmCrop("minecraft:wheat", 7, 1, 1)
             farmCrop("thermal:tomato", 10, 2, 1)
             farmCrop("thermal:corn", 9, 3, 2)
             farmCrop("thermal:eggplant", 10, 4, 1)
 
-            -- Schritte in X-Richtung zählen und bewegen (nur wenn nicht am Ende der Reihe)
             if x < 8 then
                 if forward then
                     turtle.forward()
@@ -29,7 +28,7 @@ function farmField()
 
         -- Schritte in Z-Richtung zählen und bewegen (nur wenn nicht am Ende des Feldes)
         if z < 8 then
-            turtle.turnRight() -- Immer nach rechts drehen, da wir in +z Richtung gehen
+            turtle.turnRight()
             turtle.forward()
             turtle.turnLeft()
             stepsZ = stepsZ + 1
@@ -38,7 +37,7 @@ function farmField()
 
     -- Zurück zur Startposition
     for i = 1, stepsZ do
-        turtle.turnLeft() -- Immer nach links drehen, da wir zurück in -z Richtung gehen
+        turtle.turnLeft()
         turtle.forward()
         turtle.turnRight()
     end
@@ -51,43 +50,47 @@ function farmField()
     for i = 1, stepsX do
         turtle.forward()
     end
-end
 
--- Funktion zum Ernten und Pflanzen einer bestimmten Pflanze (unverändert)
-function farmCrop(cropName, matureAge, seedSlot, height)
-    -- ... (Code aus vorheriger Antwort) ...
+    -- Umdrehen am Ende der Runde
+    turtle.turnRight()
+    turtle.turnRight()
 end
-
--- Hauptprogramm
-while true do
-    farmField()
-    sleep(10) -- Wartezeit zwischen den Durchläufen
-end
-
 
 -- Funktion zum Ernten und Pflanzen einer bestimmten Pflanze
 function farmCrop(cropName, matureAge, seedSlot, height)
-    local success, blockInfo = turtle.inspectDown()
-    if success and blockInfo.name == cropName then
-        if blockInfo.state.age == matureAge then
-            for i = 1, height do
+    local currentY = 0 -- Variable to keep track of the current Y level
+
+    for i = 1, height do
+        local success, blockInfo = turtle.inspectDown()
+        if success and blockInfo.name == cropName then
+            if blockInfo.state.age == matureAge then
                 turtle.digDown()
-                if i < height then
-                    turtle.up()
-                end
+                currentY = currentY + 1 -- Nach dem Ernten nach oben bewegen
+            else
+                break             -- Schleife abbrechen, wenn die Pflanze nicht reif ist
             end
-            turtle.select(seedSlot)
-            turtle.placeDown()
-            for i = 1, height - 1 do
-                turtle.up()
-                turtle.placeUp()
-            end
-            print(cropName .. " geerntet und neu gepflanzt")
-        else
-            print(cropName .. " wächst noch...")
         end
-    else
-        print("Kein " .. cropName .. " gefunden")
+        if i < height and currentY < height - 1 then
+            turtle.up()
+            currentY = currentY + 1
+        end
+    end
+
+    -- Nur pflanzen, wenn die Pflanze geerntet wurde (d.h. die Schleife nicht vorzeitig abgebrochen wurde)
+    if i == height then
+        turtle.select(seedSlot)
+        turtle.placeDown()
+
+        -- Move down to the original Y level
+        for i = 1, currentY do
+            turtle.down()
+        end
+
+        for i = 1, height - 1 do
+            turtle.up()
+            turtle.placeUp()
+        end
+        print(cropName .. " geerntet und neu gepflanzt")
     end
 end
 
