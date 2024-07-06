87d1caacbd90d07c846fd4e6477b60f6c240333b
SkrenJ <marius.janowski@gmail.com>
2024-07-06 15:14:09 +0200

    c

--- farm.lua
+++ farm.lua
@@ -1,7 +1,6 @@
 -- Funktion zum Ausgeben der Blockinformationen
 function printBlockInfo()
     local blockInfo = turtle.inspectDown()  -- Informationen über den Block unter der Turtle abrufen
-    if blockInfo then  -- Überprüfen, ob ein Block gefunden wurde
       print("Name:", blockInfo.name)  -- Name des Blocks ausgeben
       print("Metadaten:", blockInfo.metadata)  -- Metadaten des Blocks ausgeben (falls vorhanden)
       if blockInfo.state then  -- Überprüfen, ob Blockzustände vorhanden sind (Minecraft 1.8+)
