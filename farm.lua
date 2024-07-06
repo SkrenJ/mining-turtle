244211283098975dab70b881f74a31ff1967aa06
SkrenJ <marius.janowski@gmail.com>
2024-07-07 00:19:44 +0200

    fix

--- farm.lua
+++ farm.lua
@@ -6,40 +6,29 @@ function farmField()
 
     for z = 0, 8 do
         stepsX = 0
-        for x = 0, 8 do
-            farmCrop("minecraft:wheat", 7, 1, 1)
-            farmCrop("thermal:tomato", 10, 2, 1)
-            farmCrop("thermal:corn", 9, 3, 2)
-            farmCrop("thermal:eggplant", 10, 4, 1)
-
-            if x < 8 then
-                if forward then
-                    turtle.forward()
-                    stepsX = stepsX + 1
-                else
-                    turtle.back()
-                    stepsX = stepsX - 1
-                end
-            end
-        end
+        goLane(8, forward) -- Abarbeiten einer Spur (8 Schritte, vorwärts oder rückwärts)
 
         -- Richtung der Bewegung in X-Richtung umkehren
         forward = not forward
 
         -- Schritte in Z-Richtung zählen und bewegen (nur wenn nicht am Ende des Feldes)
         if z < 8 then
-            turtle.turnRight()
-            turtle.forward()
-            turtle.turnLeft()
+            if forward then
+                rightCurve() -- Rechtskurve
+            else
+                leftCurve()  -- Linkskurve
+            end
             stepsZ = stepsZ + 1
         end
     end
 
     -- Zurück zur Startposition
     for i = 1, stepsZ do
-        turtle.turnLeft()
-        turtle.forward()
-        turtle.turnRight()
+        if forward then
+            leftCurve()  -- Linkskurve
+        else
+            rightCurve() -- Rechtskurve
+        end
     end
 
     -- Schritte in X-Richtung rückgängig machen (in umgekehrter Richtung)
@@ -56,49 +45,80 @@ function farmField()
     turtle.turnRight()
 end
 
+-- Funktion zum Abarbeiten einer Spur
+function goLane(length, forward)
+    for x = 0, length do
+        farmCrop("minecraft:wheat", 7, 1, 1)
+        farmCrop("thermal:tomato", 10, 2, 1)
+        farmCrop("thermal:corn", 9, 3, 2)
+        farmCrop("thermal:eggplant", 10, 4, 1)
+
+        if x < length then
+            if forward then
+                turtle.forward()
+            else
+                turtle.back()
+            end
+        end
+    end
+end
+
+-- Funktion für eine Rechtskurve
+function rightCurve()
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
+end
+
+-- Funktion für eine Linkskurve
+function leftCurve()
+    turtle.turnLeft()
+    turtle.forward()
+    turtle.turnLeft()
+end
+
 -- Funktion zum Ernten und Pflanzen einer bestimmten Pflanze
 function farmCrop(cropName, matureAge, seedSlot, height)
     local currentY = 0 -- Variable to keep track of the current Y level
-  
+
     -- Ernten
     for i = 1, height do
-      local success, blockInfo = turtle.inspectDown()
-      if success and blockInfo.name == cropName then
-        if blockInfo.state.age == matureAge then
-          turtle.digDown()
+        local success, blockInfo = turtle.inspectDown()
+        if success and blockInfo.name == cropName then
+            if blockInfo.state.age == matureAge then
+                turtle.digDown()
+            else
+                break -- Schleife abbrechen, wenn die Pflanze nicht reif ist
+            end
         else
-          break  -- Schleife abbrechen, wenn die Pflanze nicht reif ist
+            break -- Schleife abbrechen, wenn kein Block oder falscher Block gefunden wurde
         end
-      else
-        break -- Schleife abbrechen, wenn kein Block oder falscher Block gefunden wurde
-      end
-  
-      -- Nach oben bewegen, wenn es noch höhere Blöcke gibt
-      if i < height then
-        turtle.up()
-        currentY = currentY + 1 -- Y-Position aktualisieren
-      end
+
+        -- Nach oben bewegen, wenn es noch höhere Blöcke gibt
+        if i < height then
+            turtle.up()
+            currentY = currentY + 1 -- Y-Position aktualisieren
+        end
     end
-  
+
     -- Pflanzen (nur wenn alle Blöcke der Pflanze geerntet wurden)
     if currentY == height - 1 then -- Überprüfen, ob alle Blöcke geerntet wurden
-      turtle.select(seedSlot)
-      turtle.placeDown()
-  
-      -- Nach unten bewegen und restliche Samen pflanzen
-      for i = 1, height - 1 do
-        turtle.down()
-        turtle.placeUp()
-      end
-      print(cropName .. " geerntet und neu gepflanzt")
+        turtle.select(seedSlot)
+        turtle.placeDown()
+
+        -- Nach unten bewegen und restliche Samen pflanzen
+        for i = 1, height - 1 do
+            turtle.down()
+            turtle.placeUp()
+        end
+        print(cropName .. " geerntet und neu gepflanzt")
     else
-      -- Zurück zur ursprünglichen Höhe, wenn nicht alle Blöcke geerntet wurden
-      for i = 1, currentY do
-        turtle.down()
-      end
+        -- Zurück zur ursprünglichen Höhe, wenn nicht alle Blöcke geerntet wurden
+        for i = 1, currentY do
+            turtle.down()
+        end
     end
-  end
-  
+end
 
 -- Hauptprogramm
 while true do
