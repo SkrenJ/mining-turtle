67a56c0f36e3707f81159bf7fcbdfc5d0e801d2d
SkrenJ <marius.janowski@gmail.com>
2024-07-06 15:22:05 +0200

    c

--- farm.lua
+++ farm.lua
@@ -1,15 +1,15 @@
 -- Funktion zum Ausgeben der Blockinformationen
 function printBlockInfo()
-    local success, blockInfo = turtle.inspectDown() -- Informationen über den Block unter der Turtle abrufen
+    --local success, blockInfo = turtle.inspectDown() -- Informationen über den Block unter der Turtle abrufen
     print("erhalten")
-    if success then                                 -- Überprüfen, ob ein Block gefunden wurde
-        print("name")
-        print("Name:", blockInfo.name)              -- Name des Blocks ausgeben
-        print("Metadaten:", blockInfo.metadata)     -- Metadaten des Blocks ausgeben (falls vorhanden)
-        -- Blockzustände (nur Minecraft 1.8+) wurden hier entfernt, da sie in den meisten Fällen nicht benötigt werden
-    else
-        print("Kein Block gefunden") -- Meldung ausgeben, wenn kein Block unter der Turtle ist
-    end
+    -- if success then                                 -- Überprüfen, ob ein Block gefunden wurde
+    --     print("name")
+    --     print("Name:", blockInfo.name)              -- Name des Blocks ausgeben
+    --     print("Metadaten:", blockInfo.metadata)     -- Metadaten des Blocks ausgeben (falls vorhanden)
+    --     -- Blockzustände (nur Minecraft 1.8+) wurden hier entfernt, da sie in den meisten Fällen nicht benötigt werden
+    -- else
+    --     print("Kein Block gefunden") -- Meldung ausgeben, wenn kein Block unter der Turtle ist
+    -- end
 end
 
 -- Hauptprogramm
