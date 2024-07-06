e9c6b7d7a7d28c7cbe7289ec0b41c027525ff166
SkrenJ <marius.janowski@gmail.com>
2024-07-06 15:41:11 +0200

    c

--- testen.lua
+++ testen.lua
@@ -2,6 +2,12 @@ function testen()
     local success, blockInfo = turtle.inspectDown() -- Informationen über den Block unter der Turtle abrufen
     print(success)
 
+    if success then
+        print("erfolg!")
+    end
+
+    print(blockInfo.name)
+
     for i = 1, 4 do
         print("Hallo Welt, ", i)
     end
